fb4246c54c20014afb2f9021e503ef9f344fff1b
Angela-Melendez-Fuentes <92596921+Angela-Melendez-Fuentes@users.noreply.github.com>
2023-08-22 23:10:25 -0600

    Add files via upload

---
+++ LectorArchivo/src/main/java/edu/upvictoria/fpoo/Main.java
@@ -0,0 +1,29 @@
+package edu.upvictoria.fpoo;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Main
+{
+
+
+    public Main() {
+
+    }
+    public static void main( String[] args )
+    {
+
+    }
+
+    public String menu() {
+
+    }
+
+
+    private String toString(String ) {
+
+    }
+}
